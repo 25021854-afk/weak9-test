296fb8b29582f3a746ada0d541ef6acb8ea401aa
25021854-afk <25021854@vnu.edu.vn>
2026-05-07 18:38:42 +0700

    Create a PR to trigger action

--- Bai9.7/src/main/java/com/example/App.java
+++ Bai9.7/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Automated Review!");
+        loiSystem.out.println("Hello, Automated Review!");
     }
 }
